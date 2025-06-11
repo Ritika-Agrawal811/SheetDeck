48db1730f55df6efc806a7d438a1bbaf86a1b806
Ritika-Agrawal811 <ritikaagrawal339@gmail.com>
2025-06-11 20:24:10 +0530

    feat: add support repository section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import CheatsheetsSection from '../components/cheatsheets-section/CheatsheetsSection';
 import HeroSection from '../components/hero-section/HeroSection';
+import Support from '../components/support-section/Support';
 
 export default function Home() {
     return (
         <>
             <HeroSection />
             <CheatsheetsSection />
+            <Support />
         </>
     );
 }
---
+++ components/support-section/Support.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import React from 'react';
+import clsx from 'clsx';
+
+// components
+import Image from 'next/image';
+import VisitRepositoryButton from './repository-button/VisitRepositoryButton';
+
+const Support = () => {
+    return (
+        <section className={clsx('py-20 bg-purple-50', 'relative')}>
+            <div className={clsx('flex flex-col gap-4 items-center', 'relative z-20')}>
+                {/* Title */}
+                <h2 className="text-6xl">
+                    Show your <span className={clsx('font-medium font-platypi text-purple-800')}>support</span>.{' '}
+                </h2>
+
+                {/* Subtitle */}
+                <p className={clsx('text-2xl', 'flex items-center gap-1')}>
+                    <span className="font-medium">
+                        Found these cheat sheets <span className="text-purple-800 font-jua">helpful</span>?
+                    </span>{' '}
+                    A <Image alt="star gif" src="/assets/star.gif" width={50} height={50} /> on GitHub goes a long way!
+                </p>
+
+                {/* Call to action button */}
+                <VisitRepositoryButton />
+            </div>
+
+            {/* Background Image */}
+            <div className={clsx('absolute left-[40%] -translate-x-[40%] bottom-0', 'z-10', 'h-1/2 w-2/5')}>
+                <Image
+                    alt="background road image"
+                    src="/assets/road.svg"
+                    width={500}
+                    height={200}
+                    quality={50}
+                    className="h-full w-full object-cover opacity-70"
+                />
+            </div>
+        </section>
+    );
+};
+
+export default Support;
