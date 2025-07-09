785a0277823bf8a8a2ae77e80b6e0072c6125f61
Ritika-Agrawal811 <ritikaagrawal339@gmail.com>
2025-07-10 00:21:04 +0530

    feat: add arrow keys navigations custom hook

--- components/ui/Dropdown.tsx
+++ components/ui/Dropdown.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useCallback } from 'react';
 import clsx from 'clsx';
 
 import { formatLabels } from '@/utils/formatLabels';
+import { useArrowKeyNavigation } from '@/hooks/useArrowKeyNavigation';
 
 // components
 import { IoChevronDownOutline } from 'react-icons/io5';
@@ -17,15 +18,10 @@ interface DropdownProps {
 }
 
 const Dropdown: React.FC<DropdownProps> = ({ label, selectedOption, setSelectedOption, data, labelStyle, labelClassname }) => {
+    const { registerItemRef, handleKeysNavigation, focusFirstItem, setActiveIndex } = useArrowKeyNavigation(data.length, {
+        orientation: 'vertical',
+    });
     const [open, setOpen] = useState(false);
-
-    const filteredData = data.filter((item, index) => data.indexOf(item) === index);
-
-    const setSelectedOptionHandler = (event: React.MouseEvent<HTMLLIElement>, option: string) => {
-        event.stopPropagation();
-        setSelectedOption(option);
-        setOpen(false);
-    };
 
     const closeExpandedOptions = useCallback(() => {
         setOpen(false);
@@ -42,6 +38,25 @@ const Dropdown: React.FC<DropdownProps> = ({ label, selectedOption, setSelectedO
         };
     }, [open, closeExpandedOptions]);
 
+    const setSelectedOptionHandler = (event: React.MouseEvent<HTMLLIElement>, option: string) => {
+        event.stopPropagation();
+        setSelectedOption(option);
+        setOpen(false);
+    };
+
+    const parentKeyDownHandler = () => {
+        focusFirstItem();
+        setActiveIndex(0);
+    };
+
+    const itemKeyDownHandler = (event: React.KeyboardEvent, option: string) => {
+        if (event.key === 'Enter' || event.key === ' ') {
+            event.stopPropagation();
+            setSelectedOption(option);
+            setOpen(false);
+        }
+    };
+
     return (
         <div className="relative" aria-label={`Select a ${label} for cheatsheet`}>
             <button
@@ -57,6 +72,7 @@ const Dropdown: React.FC<DropdownProps> = ({ label, selectedOption, setSelectedO
                     'focus:outline-none focus:border-transparent focus:ring-2 focus:ring-blue-500'
                 )}
                 onClick={() => setOpen((prev) => !prev)}
+                onKeyDown={parentKeyDownHandler}
             >
                 <span
                     className={clsx('p-2 xl:p-2.5', 'rounded-l-md border-r border-gray-200', 'text-sm xl:text-base', labelClassname)}
@@ -84,8 +100,9 @@ const Dropdown: React.FC<DropdownProps> = ({ label, selectedOption, setSelectedO
                     'text-center divide-y divide-gray-200',
                     open ? 'block' : 'hidden'
                 )}
+                onKeyDown={handleKeysNavigation}
             >
-                {filteredData.map((item) => {
+                {data.map((item, index) => {
                     const isSelected = item === selectedOption;
                     return (
                         <li
@@ -99,6 +116,8 @@ const Dropdown: React.FC<DropdownProps> = ({ label, selectedOption, setSelectedO
                                 isSelected ? 'bg-emerald-700 text-white hover:bg-emerald-800' : 'hover:bg-gray-100'
                             )}
                             onClick={(event) => setSelectedOptionHandler(event, item)}
+                            onKeyDown={(event) => itemKeyDownHandler(event, item)}
+                            ref={(elem) => registerItemRef(elem, index)}
                         >
                             {formatLabels(item)}
                         </li>
