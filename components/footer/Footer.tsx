77b91f7d8b85c21bf992b38e723ee9bf195c7180
Ritika-Agrawal811 <ritikaagrawal339@gmail.com>
2025-07-17 00:43:51 +0530

    fix: responsivity for large screens

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -11,11 +11,11 @@ const Footer = () => {
         <footer
             role="contentinfo"
             className={clsx(
-                'w-full md:w-11/12 2xl:w-4/5 max-w-screen-3xl mx-auto',
-                'p-6 mt-16 md:mb-6',
+                'w-full md:w-11/12 2xl:w-[85%] 3xl:w-4/5 max-w-screen-3xl mx-auto',
+                'p-6 xl:p-5 3xl:p-6 mt-16 md:mb-6',
                 'bg-primary md:rounded-xl',
                 'border-b border-primary',
-                'text-sm md:text-base',
+                'text-sm md:text-base 4xl:text-lg',
                 'flex flex-col sm:flex-row items-start sm:items-center justify-between',
                 'gap-4 sm:gap-0'
             )}
